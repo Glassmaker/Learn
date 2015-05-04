4315b9aeacedcc44cefd0ea6ad81eb59657b6962
Glassmaker <JeanGlassmaker@mail.com>
2015-05-04 21:31:54 +0300

    Adds basic nbt functions

--- src/main/java/mod/learn/tileentities/TileEntityGate.java
+++ src/main/java/mod/learn/tileentities/TileEntityGate.java
@@ -1,7 +1,35 @@
 package mod.learn.tileentities;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityGate extends TileEntity {
+
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+        NBTTagCompound nbt = pkt.func_148857_g();
+        if (nbt != null)
+            readFromNBT(nbt);
+    }
+
+    @Override
+    public Packet getDescriptionPacket() {
+        NBTTagCompound nbt = new NBTTagCompound();
+        writeToNBT(nbt);
+        return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbt);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+    }
 
 }
