5b1df183bc99b8785fa4ed3e775bf2615c78b5b4
Glassmaker <JeanGlassmaker@mail.com>
2015-05-04 19:02:20 +0300

    Adds textures to sides

--- src/main/java/mod/learn/blocks/BlockGate.java
+++ src/main/java/mod/learn/blocks/BlockGate.java
@@ -2,7 +2,9 @@ package mod.learn.blocks;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import mod.learn.LearnTabs;
@@ -10,6 +12,10 @@ import mod.learn.misc.LearnRef;
 import mod.learn.tileentities.TileEntityGate;
 
 public class BlockGate extends BlockContainer {
+
+    // Will be using blockIcon for the front;
+    protected IIcon iconBack;
+    protected IIcon iconSide;
 
     public BlockGate() {
         super(Material.iron);
@@ -21,6 +27,28 @@ public class BlockGate extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World world, int meta) {
         return new TileEntityGate();
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister register) {
+
+        iconBack = register.registerIcon(LearnRef.MODID + ":gate_back");
+        iconSide = register.registerIcon(LearnRef.MODID + ":gate_side");
+
+        super.registerBlockIcons(register); // registers blockIcon
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta) {
+
+        switch (side) {
+            case 3:
+                return blockIcon; // front
+            case 2:
+                return iconBack;
+            default:
+                return iconSide;
+        }
     }
 
 }
